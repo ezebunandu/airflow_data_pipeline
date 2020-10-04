1740066a2592089d41f3dbb3b312e3a1b92cfa98
Sam Ezebunandu <samuel.ezebunandu@outlook.com>
2020-10-04 12:50:50 -0600

    Operator and dag for data quality check

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -2,21 +2,41 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+
 class DataQualityOperator(BaseOperator):
+    """Operator to perform data quality check
+    by confirming that there are records (rows) in
+    dimension and fact tables.
+    """
 
     ui_color = '#89DA59'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 tables=None,
+                 redshift_conn='',
                  *args, **kwargs):
 
-        super(DataQualityOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        super().__init__(*args, **kwargs)
+        self.tables = tables or []
+        self.redshift_conn = redshift_conn
 
     def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
+        redshift = PostgresHook(self.redshift_conn)
+        self.log.info('Starting data quality checks')
+
+        for table in self.tables:
+            self.log.info(f'Data quality check on {table}')
+            records = redshift.get_records(f"SELECT COUNT(*) FROM {table}")
+            num_records = records[0][0]
+
+            if not records or len(records[0]) < 1:
+                raise ValueError(f"Data quality check failed.\
+                                 {table} returned no results")
+
+            if num_records < 1:
+                raise ValueError(f"Data quality check failed.\
+                                 {table} contained 0 rows")
+
+            self.log.info(f"Data quality check on table {table} passed with\
+                          {num_records} records")
