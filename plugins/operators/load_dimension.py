c4e912fe87fba6d061215d5fa464d034f30da84e
Sam Ezebunandu <samuel.ezebunandu@outlook.com>
2020-10-04 19:50:31 -0600

    clear_table flag in LoadDimensionOperator to turn append mode on/off

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -2,6 +2,7 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+
 class LoadDimensionOperator(BaseOperator):
 
     ui_color = '#80BD9E'
@@ -10,20 +11,20 @@ class LoadDimensionOperator(BaseOperator):
     def __init__(self,
                  redshift_conn='',
                  table='',
-                 append_mode=False,
+                 clear_table=False,
                  sql='',
                  *args, **kwargs):
 
         super().__init__(*args, **kwargs)
-        self.redshift_conn=redshift_conn
-        self.table=table
-        self.append_mode=append_mode
-        self.sql=sql
+        self.redshift_conn = redshift_conn
+        self.table = table
+        self.clear_table = clear_table
+        self.sql = sql
 
     def execute(self, context):
         redshift = PostgresHook(self.redshift_conn)
 
-        if not self.append_mode:
+        if self.clear_table:
             self.log.info(f'Not in append mode; clearing the {self.table} before inserting new data')
             redshift.run(f'DELETE FROM {self.table};')
 
